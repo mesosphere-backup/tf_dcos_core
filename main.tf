a2bf5726a5573a5e2cc232eb5dc7a55eafd43947
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2017-08-28 14:04:41 -0700

    add support for dcos_ip_detect_contents

--- main.tf
+++ main.tf
@@ -71,6 +71,7 @@ template = "${file("${path.module}/dcos-versions/${var.dcos_version}/${var.role}
     dcos_cluster_docker_registry_url = "${var.dcos_cluster_docker_registry_url}"
     dcos_rexray_config = "${var.dcos_rexray_config}"
     dcos_ip_detect_public_contents = "${var.dcos_ip_detect_public_contents}"    
+    dcos_ip_detect_contents = "${var.dcos_ip_detect_contents}"
     dcos_cluster_docker_registry_enabled = "${var.dcos_cluster_docker_registry_enabled}"
     dcos_enable_docker_gc = "${var.dcos_enable_docker_gc}"
     dcos_staged_package_storage_uri = "${var.dcos_staged_package_storage_uri}"
