1d8e5d3f57d184dd529756dd0cbe1b88fb4df680
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2017-09-19 10:10:47 -0700

    removed extra space in upgrade instruction

--- dcos-versions/1.9.0-rc3/dcos-bootstrap/templates/upgrade/run.sh
+++ dcos-versions/1.9.0-rc3/dcos-bootstrap/templates/upgrade/run.sh
@@ -74,7 +74,7 @@ ${dcos_package_storage_uri == "" ? "" : "  package_storage_uri: ${dcos_package_s
 EOF
 cp /tmp/ip-detect genconf/.
 OVERRIDE_PREVIOUS_DCOS_VERSION=${dcos_previous_version}
-PREVIOUS_DCOS_VERSION=$(grep -a "'dcos_version':" "$(ls -altr dcos_generate_config.*  | tail -1 | awk '{ print $9 }')" | cut -d ":" -f2 | sed 's/,$//' | sed s/\'//g)
+PREVIOUS_DCOS_VERSION=$(grep -a "'dcos_version':" "$(ls -altr dcos_generate_config.* | tail -1 | awk '{ print $9 }')" | cut -d ":" -f2 | sed 's/,$//' | sed s/\'//g)
 curl -o dcos_generate_config.${dcos_version}.sh ${dcos_download_path}
 bash dcos_generate_config.${dcos_version}.sh --generate-node-upgrade-script $${OVERRIDE_PREVIOUS_DCOS_VERSION:-$${PREVIOUS_DCOS_VERSION}}
 rm -fr genconf/serve/upgrade/current
