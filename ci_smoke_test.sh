4ff8463251df94eb789e92e543b850c7d0df9218
Charles Provencher <cprovencher@mesosphere.com>
2018-04-26 16:08:00 -0700

    Add ci_smoke_test.sh

---
+++ ci_smoke_test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -euxo pipefail
+
+# This part checks that the local version of tf_dcos_core does not break terraform-dcos
+mkdir .tf_dcos_core
+mv * .tf_dcos_core
+git remote add terraform-dcos https://github.com/dcos/terraform-dcos.git
+git fetch terraform-dcos
+# change this line once ci_script branch is merged in terraform-dcos
+git reset --hard terraform-dcos/ci_script
+find . -name '*.tf' -exec sed -i 's|github.com/dcos/tf_dcos_core|../.tf_dcos_core|g' {} \;
+chmod +x ci_smoke_test.sh
+./ci_smoke_test.sh
+
+# This part checks that tf_dcos_core works on its own
+mv terraform .tf_dcos_core/terraform
+cd .tf_dcos_core
+export TERRAFORM_PATH=${PWD}/terraform
+${TERRAFORM_PATH} init
+for dir in ./dcos-versions/*
+do
+    version=${dir%*/}
+    version=${version##*/}
+    for dir2 in ./dcos-versions/${version}/*
+    do
+        role=${dir2%*/}
+        role=${role##*/}
+        ${TERRAFORM_PATH} validate -var "role=${role}" -var "dcos_version=${version}"
+        ${TERRAFORM_PATH} plan -var "role=${role}" -var "dcos_version=${version}"
+    done
+done
