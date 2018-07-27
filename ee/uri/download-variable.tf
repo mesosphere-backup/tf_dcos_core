a4986511067b740a100c427164e24a03a6fe1a6b
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2018-07-27 20:00:10 +0200

    ee/open: added support for DC/OS 1.11.4

--- ee/uri/download-variable.tf
+++ ee/uri/download-variable.tf
@@ -7,6 +7,7 @@ variable "dcos_download_path" {
    "1.11.1"     = "https://downloads.mesosphere.com/dcos-enterprise/stable/commit/a7908b05bf226783484a97463eacd5d1dcbaa9c6/dcos_generate_config.ee.sh"
    "1.11.2"     = "https://downloads.mesosphere.com/dcos-enterprise/stable/commit/ad1aa93db99afacfa0099e419667ab60519c081e/dcos_generate_config.ee.sh"
    "1.11.3"     = "https://downloads.mesosphere.com/dcos-enterprise/stable/commit/5fdf7dfae5c4bdb233e8e6a7f198ef6b08c5b067/dcos_generate_config.ee.sh"
+   "1.11.4"     = "https://downloads.mesosphere.com/dcos-enterprise/stable/commit/2153e9ad7e4e64b57b7c7e323d40b1cdde7f7428/dcos_generate_config.ee.sh"
    "master"     = "https://downloads.mesosphere.com/dcos-enterprise/testing/master/dcos_generate_config.ee.sh"
   }
 }
